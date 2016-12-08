3a31e6a333d64bf9168e706f6622d96c7deb2fbb
Julius Peinelt <julius.peinelt@mailbox.org>
2016-12-08 17:49:42 +0100

    started with landing page

---
+++ views/site/index.php
@@ -0,0 +1,94 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Charter';
+?>
+<div class="site-index">
+
+
+
+    <div class="body-content">
+
+        <div class="jumbotron" style="background-color:#0000FF">
+            <h1>Data Re-Use Charter</h1>
+
+            <p class="lead">Supporting research collaboration on Cultural Heritage data</p>
+
+            <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started now</a></p>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <p>Making Cultural Heritage data easier to access, more sustainable, reproducible and citable.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>Image...</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="jumbotron" style="background-color:#0000F3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <p>The funny five pictures</p>
+                    </div>
+                    <div class="col-lg-6">
+                        <p>All partners in the Charter benefit equally from the greater visibility and communication made possible by joining forces.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <h2>Collaboration</h2>
+                    <p>The Charter helps you find new partners and define the goals of your reasearch collaboration.</p>
+                    <p>Image</p>
+                </div>
+                <div class="col-lg-4">
+                    <h2>Collaboration</h2>
+                    <p>The Charter helps you find new partners and define the goals of your reasearch collaboration.</p>
+                    <p>Image</p>
+                </div>
+                <div class="col-lg-4">
+                    <h2>Collaboration</h2>
+                    <p>The Charter helps you find new partners and define the goals of your reasearch collaboration.</p>
+                    <p>Image</p>
+                </div>
+            </div>
+        </div>
+        <div class="jumbotron" style="background-color:#DDDDDD">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                            <div class="list-group">
+                            <a href="#" class="list-group-item">
+                                <h4 class="list-group-item-heading">First List Group Item Heading</h4>
+                                <p class="list-group-item-text">List Group Item Text</p>
+                            </a>
+                            <a href="#" class="list-group-item">
+                                <h4 class="list-group-item-heading">Second List Group Item Heading</h4>
+                                <p class="list-group-item-text">List Group Item Text</p>
+                            </a>
+                            <a href="#" class="list-group-item">
+                                <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+                                <p class="list-group-item-text">List Group Item Text</p>
+                            </a>
+                            <a href="#" class="list-group-item">
+                                <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+                                <p class="list-group-item-text">List Group Item Text</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        image
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
